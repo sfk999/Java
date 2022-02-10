563a0ef58bf3bb300ed9ed0ed09582e65769e46a
sfk999 <avm-expert@ya.ru>
2022-02-10 22:26:58 +0300

    home-work1

--- Main.java
+++ Main.java
@@ -1,21 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int productPrice = 100;
-        productPrice = productPrice + 1;
+//        int productPrice = 100;
+//        int productPrice2 = 200;
+//
+//        int productsNumber = 10;
+//
+//        int sum = productPrice * productsNumber;
+//        System.out.println("Сумма = " + sum + " руб.");
+//
+//        String myString = " My automation string 123 ";
+//
+//        System.out.println("Строка без пробелов " + myString.trim());
+//        int stringLenght = myString.trim().length();
+//
+//        System.out.println("Длина строки " + stringLenght);
+//
+//        System.out.println("Строка содержит" );
+//        System.out.println("price1 != price2:" + (productPrice != productPrice2));
 
-        int productsNumber = 10;
+        String string1 = "First string";
+//        Str string2 = "First string";
+////        System.out.println("Строки равны " + string1.equals(string2));ing
 
-        int sum = productPrice * productsNumber;
-        System.out.println("Сумма = " + sum + " руб.");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите строку: ");
+//        String stringFromConsole = scanner.nextLine();
+//        System.out.println("Вы ввели строку: " + stringFromConsole);
 
-        String myString = " My automation string 123 ";
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите ваше имя: ");
+//        String stringName = scanner.nextLine();
+//        System.out.println("Привет, " + stringName + "!");
 
-        System.out.println("Строка без пробелов " + myString.trim());
-        int stringLenght = myString.trim().length();
+        if (string1.equals("First string")) {
+            System.out.println("Первая строка");
+        }
 
-        System.out.println("Длина строки " + stringLenght);
-
-        System.out.println("Строка содержит" );
     }
 }
